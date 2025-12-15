704f7c8a9da10c76339840d3e54b5768b5049338
HattourWejden <wejden.hattour@eniso.u-sousse.tn>
2025-12-15 15:54:17 +0100

    feat: implement Order Service as Kafka producer

--- inventory-service/src/main/java/com/lab/inventory/repository/InventoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lab.inventory.repository;
-
-import com.lab.inventory.model.InventoryItem;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InventoryRepository extends MongoRepository<InventoryItem, String> {
-}
